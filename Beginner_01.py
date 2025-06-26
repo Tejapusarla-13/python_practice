897e89462e4f6b959bb7f8ae7098cbe432206edb
teja <pteja0813@gmail.com>
2025-06-26 21:20:03 +0100

    Practice Exercise

--- Beginner_01.py
+++ Beginner_01.py
@@ -1,6 +1,8 @@
 # Exercise 1: Calculate the multiplication and sum of two numbers
 
-a=10
-b=6
+a=input("enter the first number")
+b=input("enter the second number")
+a=int(a)
+b=int(b)
 print(a*b)
 print(a+b)
