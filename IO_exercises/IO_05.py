a6622e0bd5974239bb47d56ca578379fb6e5e98b
teja <pteja0813@gmail.com>
2025-06-27 23:13:02 +0100

    practice Exercise

---
+++ IO_exercises/IO_05.py
@@ -0,0 +1,9 @@
+#Exercise 5: Accept a list of 5 float numbers as an input from the user
+
+list1=[]
+
+for i in range(5):
+    num=float(input("enter a decimal number"))
+    list1.append(num)
+
+print(list1)
