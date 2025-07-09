7c2100d9a60b1ffda81be6cb5a15b55503c15ef4
teja <pteja0813@gmail.com>
2025-07-09 22:33:13 +0100

    Practice Exercise

--- Beginner_03.py
+++ Beginner_03.py
@@ -1,7 +1,8 @@
 # Exercise 3: Print characters present at an even index number
+def str_slice(str1):
+    print("Orginal String is  PYnative")
+    print("Printing only even index chars")
+    for i in str1[::2]:
+        print(i)
 
-str1= "Pynative"
-print("Orginal String is  PYnative")
-print("Printing only even index chars")
-for i in str1[::2]:
-    print(i)
+str_slice("Pynative")
