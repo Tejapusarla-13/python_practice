978f25dc0d963e172aa1e202f529c051c41bfb92
teja <pteja0813@gmail.com>
2025-07-10 22:30:18 +0100

    Practice Exercise

--- Beginner_07.py
+++ Beginner_07.py
@@ -1,23 +1,28 @@
 # Exercise 7: Find the number of occurrences of a substring in a string
 
-str_x = "Emma is good developer. Emma is a writer"
+def no_of_ocurrences(str_x):
+     
 
-str1=""
-wrd=[]
-for i in str_x:
-    if i !=" ":
-        str1+=i
-    if i == " ":
-        wrd.append(str1)
-        str1=""
-wrd.append(str1)
-print(wrd)
-my_dict={}
-for i in wrd:
-    if i not in my_dict:
-        my_dict[i]=0
-    if i in my_dict:
-        my_dict[i]+=1
-print(my_dict)
+    str1=""
+    wrd=[]
+    for i in str_x:
+        if i !=" ":
+            str1+=i
+        if i == " ":
+            wrd.append(str1)
+            str1=""
+    wrd.append(str1)
+    print(wrd)
+    my_dict={}
+    for i in wrd:
+        if i not in my_dict:
+            my_dict[i]=0
+        if i in my_dict:
+            my_dict[i]+=1
+    print(my_dict)
 
-print("Emma appeared",my_dict["Emma"],"times")
+    print("Emma appeared",my_dict["Emma"],"times")
+    
+    return
+    
+no_of_ocurrences(str_x="Emma is good developer. Emma is a writer")
