e18827991db6ca94ece0c6a1e6807f72fce200c4
teja <pteja0813@gmail.com>
2025-07-10 22:24:55 +0100

    Practice Exercise

--- Beginner_06.py
+++ Beginner_06.py
@@ -1,8 +1,12 @@
 # Exercise 6: Display numbers divisible by 5
 
-lis=[10, 20, 33, 46, 55]
-print("Given list is  [10, 20, 33, 46, 55]")
-print("Divisible by 5")
-for i in lis:
-    if i%5==0:
-        print(i)
+def divisilble_5(lis):
+    print("Given list is",lis)
+    print("Divisible by 5")
+    for i in lis:
+        if i%5==0:
+            print(i)
+    return
+
+divisilble_5(lis=[10, 20, 33, 46, 55])
+
