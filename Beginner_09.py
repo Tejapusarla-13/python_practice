ad5c2f11e92d803522ee93b95e3694109398574b
teja <pteja0813@gmail.com>
2025-07-11 11:36:59 +0100

    Practice Exercise

--- Beginner_09.py
+++ Beginner_09.py
@@ -1,7 +1,11 @@
 # Exercise 9: Check Palindrome Number
-number=input("enter the number to palindrome")
 
-if number[0]== number[-1]:
-    print("the number is palindrome")
-else:
-    print("the number is not palindrome")
+def palindrome(num):
+    num=str(num)
+    if num[0]== num[-1]:
+        print("the number is palindrome")
+    else:
+        print("the number is not palindrome")
+    return
+    
+palindrome(num=232)
