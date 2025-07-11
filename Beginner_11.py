12e76e7b7f9a11fe821de6b4a02f83cccfbb25ba
teja <pteja0813@gmail.com>
2025-07-11 12:06:32 +0100

    Practice Exercise

--- Beginner_11.py
+++ Beginner_11.py
@@ -1,8 +1,14 @@
-number = 7536
-print("Given number", number)
-while number > 0:
-    # get the last digit
-    digit = number % 10
-    # remove the last digit and repeat the loop
-    number = number // 10
-    print(digit, end=" ")
+# Exercise 11: Get each digit from a number in the reverse order.
+
+def rev_num(num):
+    num1=num
+    rev_num=0
+    while num1>0:
+        rem=num1%10
+        rev_num1=rev_num*10+rem
+        num1=num1//10
+    
+    print(rev_num1)
+    return
+    
+rev_num(1234)
